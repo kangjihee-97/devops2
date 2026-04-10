10fc2ea72db5da986ad1f7be003cf4db8fa609cc
kangjihee-97 <oz65508966@gmail.com>
2026-04-10 11:02:27 +0900

    aws_devops2 v1.0

---
+++ aws_devops2/src/main/java/org/cloud/AwsDevops2Application.java
@@ -0,0 +1,13 @@
+package org.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevops2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevops2Application.class, args);
+	}
+
+}
